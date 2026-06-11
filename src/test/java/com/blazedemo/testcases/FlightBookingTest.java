6a1cd4c621782543070e3644d3aaebeed08b655f
M MYTHILI <M MYTHILI@DESKTOP-IL0VP7Q.lan>
2026-06-11 16:19:44 +0530

    commit

--- src/test/java/com/blazedemo/testcases/FlightBookingTest.java
+++ src/test/java/com/blazedemo/testcases/FlightBookingTest.java
@@ -1,7 +1,7 @@
 
 package com.blazedemo.testcases;
 
-import com.blazedemo.base.BaseTest;
+/*import com.blazedemo.base.BaseTest;
 import com.blazedemo.pages.*;
 import com.blazedemo.utilities.CSVDataReader;
 import org.testng.Assert;
@@ -52,7 +52,70 @@ public class FlightBookingTest extends BaseTest {
         // Assert that the message contains the expected text
         Assert.assertTrue(confirm.isSuccess(), "Purchase confirmation not found!");
     }
+    
+*/
+
+
+
+import com.blazedemo.base.BaseTest;
+import com.blazedemo.pages.*;
+import com.blazedemo.utilities.CSVDataReader;
+import com.blazedemo.utilities.ScreenshotUtil; // ✅ added import
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class FlightBookingTest extends BaseTest {
+
+    @DataProvider(name = "csvData")
+    public Object[][] getData() {
+        return CSVDataReader.getData("flightData.csv");
+    }
+
+    @Test(dataProvider = "csvData")
+    public void TC_FlightBooking(String tcid, String from, String to,
+                                 String name, String address, String city,
+                                 String state, String zip, String card,
+                                 String month, String year) {
+
+        System.out.println("Executing: " + tcid);
+
+        HomePage home = new HomePage(driver);
+        FlightResultsPage results = new FlightResultsPage(driver);
+        PurchasePage purchase = new PurchasePage(driver);
+        ConfirmationPage confirm = new ConfirmationPage(driver);
+
+        // Step 1: Select cities
+        home.selectDeparture(from);
+        home.selectDestination(to);
+        home.clickFindFlights();
+        ScreenshotUtil.captureScreenshot(driver, "Cities_Selected_" + tcid);
+
+        // Step 2: Choose flight and fill form
+        results.selectFirstFlight();
+        purchase.enterName(name);
+        purchase.enterAddress(address);
+        purchase.enterCity(city);
+        purchase.enterState(state);
+        purchase.enterZip(zip);
+        purchase.enterCard(card);
+        purchase.enterMonth(month);
+        purchase.enterYear(year);
+        purchase.enterNameOnCard(name);
+        ScreenshotUtil.captureScreenshot(driver, "Form_Filled_" + tcid);
+
+        // Step 3: Confirm booking
+        purchase.clickPurchase();
+
+        // Print the actual confirmation message
+        String message = confirm.getConfirmationText();
+        System.out.println("Confirmation Message: " + message);
+        ScreenshotUtil.captureScreenshot(driver, "Confirmation_Page_" + tcid);
+
+        // Assert that the message contains the expected text
+        Assert.assertTrue(confirm.isSuccess(), "Purchase confirmation not found!");
+    }
 }
+
 
 
 
---
+++ src/test/java/com/blazedemo/utilities/ScreenshotUtil.java
@@ -0,0 +1,32 @@
+package com.blazedemo.utilities;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenshotUtil {
+
+    public static String captureScreenshot(WebDriver driver, String fileName) {
+        // Timestamp for uniqueness
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String screenshotPath = System.getProperty("user.dir") + "/screenshots/" + fileName + "_" + timestamp + ".png";
+
+        // Take screenshot
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+        try {
+            FileUtils.copyFile(srcFile, new File(screenshotPath));
+            System.out.println("Screenshot saved at: " + screenshotPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return screenshotPath;
+    }
+}
